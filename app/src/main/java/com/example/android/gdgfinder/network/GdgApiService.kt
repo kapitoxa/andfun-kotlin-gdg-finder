62ac9b931388be1dfc50c7150f625befdea953e7
Anton Borodaenko <k4p1toxa@gmail.com>
2020-08-07 09:32:17 +0300

    Change GDG List url

--- app/src/main/java/com/example/android/gdgfinder/network/GdgApiService.kt
+++ app/src/main/java/com/example/android/gdgfinder/network/GdgApiService.kt
@@ -9,10 +9,10 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 
-private const val BASE_URL = "https://developers.google.com/community/gdg/directory/"
+private const val BASE_URL = "https://gdg.community.dev/api/"
 
 interface GdgApiService {
-    @GET("directory.json")
+    @GET("chapter_region")
     fun getChapters():
     // The Coroutine Call Adapter allows us to return a Deferred, a Job with a result
             Deferred<GdgResponse>
